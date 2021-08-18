f7dfad60baf9889052da8d9404914324c858be9b
Bruno <brunosabia1@gmail.com>
2021-08-18 15:55:04 -0300

    DevAcademy- Day8

---
+++ Aula8/fake_instagram_base_eventos/js/script.js
@@ -0,0 +1,46 @@
+let btnMore = document.querySelector('main.container button#more');
+let btnHeart = document.querySelector('main.container .likes img');
+let search = document.querySelector('header .container .busca form');
+let container = document.querySelector('main.container #post');
+let parentDiv = document.querySelector('main.container')
+let stop = document.querySelector('main.container .stop')
+
+//console.log(search);
+//console.log(btnHeart);
+console.log(btnMore);
+
+//ex 1
+btnMore.onclick = (event) => {
+    container.nextElementSibling;
+    let newPost = container.cloneNode(true);
+    parentDiv.insertBefore(newPost, stop);
+}
+
+//ex 2
+btnHeart.onclick = (event) => {
+        event.target.nextElementSibling;
+    if (btnHeart.getAttribute('src') == "img/icons/heart.svg") {
+
+        btnHeart.src = "img/red-heart.png";
+        
+
+    } else {
+        btnHeart.src = "img/icons/heart.svg";
+        
+        
+    }
+}
+
+//ex 3
+search.onmouseover = (event) => {
+    search.style.cssText = "box-shadow:0px 1px 3px black;";
+}
+search.onmouseleave = (event) => {
+    search.style.cssText = "box-shadow:0px;";
+}
+
+//ex 4
+btnMore.onmouseover = (event) => {
+    btnMore.style = "cursor:pointer"
+}
+
