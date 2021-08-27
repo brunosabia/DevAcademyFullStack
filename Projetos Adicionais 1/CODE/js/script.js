a9fc93a79199d6cf2b0be50250f02238c9cdcb1a
Bruno <brunosabia1@gmail.com>
2021-08-26 21:44:18 -0300

    DevAcademy- Day14 Additional Project Started

---
+++ Projetos Adicionais 1/CODE/js/script.js
@@ -0,0 +1,14 @@
+
+//adiciona classe ativa no clique
+const dot = document.querySelector('.dot');
+const sec = document.querySelector('.sec');
+dot.onclick = function(){
+    dot.classList.toggle('active');
+    sec.classList.toggle('active');
+}
+
+document.addEventListener("mousemove", function (e) {
+    dot.style.left = e.pageX+'px';
+    dot.style.top = e.pageY+'px';
+    
+})
