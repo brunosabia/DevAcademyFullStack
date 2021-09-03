cc8773d91496d832e3c240ba382ed45d92b15cad
Bruno <brunosabia1@gmail.com>
2021-09-03 08:20:44 -0300

    Adding Java exercises

---
+++ Aula19/SistemaFinanceiro/src/ContaPoupança.java
@@ -0,0 +1,30 @@
+public class ContaPoupança {
+
+    private String saldo;
+    private String dataMinSaque;
+    private String titular;
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getDataMinSaque() {
+        return dataMinSaque;
+    }
+
+    public void setDataMinSaque(String dataMinSaque) {
+        this.dataMinSaque = dataMinSaque;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+}
