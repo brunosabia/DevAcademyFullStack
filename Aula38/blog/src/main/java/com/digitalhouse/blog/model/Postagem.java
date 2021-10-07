373fe6f46418fc04ab8e3ea6cb2333d793aa0b38
Bruno <brunosabia1@gmail.com>
2021-10-07 16:21:44 -0300

    Connected with DB

--- Aula38/blog/src/main/java/com/digitalhouse/blog/model/Postagem.java
+++ Aula38/blog/src/main/java/com/digitalhouse/blog/model/Postagem.java
@@ -1,6 +1,8 @@
 package com.digitalhouse.blog.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 import java.util.Date;
@@ -9,6 +11,7 @@ import java.util.Date;
 public class Postagem  {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String titulo;
     private String texto;
